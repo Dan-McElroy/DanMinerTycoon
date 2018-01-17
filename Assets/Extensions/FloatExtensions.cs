9b312c3e8518bd6ecfbf19bffec5816ef253d84d
Dan McElroy <daniel@mcelroys.net>
2018-01-17 19:02:42 +0000

    Commented ApproximatelyEquals method.

--- Assets/Extensions/FloatExtensions.cs
+++ Assets/Extensions/FloatExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /// <summary>
 /// A static class containing extension methods for <see cref="float"/>s.
 /// </summary>
 public static class FloatExtensions
 {
+    /// <summary>
+    /// Checks whether two <see cref="float"/> values are approximately equal.
+    /// </summary>
+    /// <param name="firstValue">The first float value to compare.</param>
+    /// <param name="secondValue">The second float value to compare.</param>
     public static bool ApproximatelyEquals(this float firstValue, float secondValue)
     {
         var epsilon = Math.Max(Math.Abs(firstValue), Math.Abs(secondValue)) * 1E-15;
