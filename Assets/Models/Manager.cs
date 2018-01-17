262123927bc64ef3f93624bfdb1b643e8da8a4b6
Dan McElroy <daniel@mcelroys.net>
2018-01-17 20:09:21 +0000

    Added Enabled value to miner, and added mouse click to toggle.

--- Assets/Models/Manager.cs
+++ Assets/Models/Manager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// A component that oversees <see cref="Worker"/>s in a <see cref="Pipeline"/>.
@@ -6,25 +7,87 @@
 public class Manager : MonoBehaviour
 {
     /// <summary>
+    /// Private variable for <see cref="Enabled"/>
+    /// </summary>
+    /// <remarks>
+    /// Not for general use, <see cref="Enabled"/>
+    /// should be used to access and mutate this
+    /// value.
+    /// </remarks>
+    [SerializeField]
+    [Obsolete("Use property Enabled instead.")]
+    private bool _enabled;
+
+    /// <summary>
+    /// The renderer for the Manager sprite.
+    /// </summary>
+    private SpriteRenderer Renderer { get; set; }
+
+// Disables warning for unassigned variables, as these
+// variables will be set in the Unity editor.
+#pragma warning disable 0649
+
+    /// <summary>
+    /// Sprite indicating that the manager is disabled.
+    /// </summary>
+    [SerializeField]
+    private Sprite DisabledSprite;
+
+    /// <summary>
+    /// Sprite indicating that the manager is enabled.
+    /// </summary>
+    [SerializeField]
+    private Sprite EnabledSprite;
+
+#pragma warning restore 0649
+
+    /// <summary>
     /// The <see cref="Pipeline"/> this <see cref="Manager"/> oversees.
     /// </summary>
     [SerializeField]
     private Pipeline Assignment;
 
     /// <summary>
+    /// Indicates whether or not the manager has been enabled.
+    /// </summary>
+    [SerializeField]
+    private bool Enabled   
+    {
+// Disable warnings for using Obsolete property.
+#pragma warning disable 0618
+        get { return _enabled; }
+        set
+        {
+            _enabled = value;
+            Renderer.sprite = _enabled
+                ? EnabledSprite
+                : DisabledSprite;
+        }
+#pragma warning restore 0618
+    }
+
+    /// <summary>
     /// Called at the start of this component's lifecycle, this
-    /// method finds its related <see cref="Pipeline"/>.
+    /// method finds its related components.
     /// </summary>
     public void Start()
     {
         Assignment = gameObject.GetComponentInParent<Pipeline>();
+        Renderer = gameObject.GetComponent<SpriteRenderer>();
+        // Ensures correct sprite is rendered.
+        Enabled = Enabled;
     }
 
     /// <summary>
-    /// Checks for idle <see cref="Worker"/>s, and re-engages them.
+    /// If the manager is enabled, checks for idle <see cref="Worker"/>s, 
+    /// and re-engages them.
     /// </summary>
     public void Update()
     {
+        if (!Enabled)
+        {
+            return;
+        }
         foreach (var worker in Assignment.Workers)
         {
             if (worker.State == WorkerState.Idle)
@@ -32,5 +95,10 @@ public class Manager : MonoBehaviour
                 worker.BeginWork();
             }
         }
+    }
+
+    public void OnMouseDown()
+    {
+        Enabled = !Enabled;    
     }
 }
