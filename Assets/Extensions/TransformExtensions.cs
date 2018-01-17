6f924b2b6e94df0c66ab967943bcb8a77c9e8fdc
Dan McElroy <daniel@mcelroys.net>
2018-01-17 18:17:39 +0000

    Commented TransformExtensions.

--- Assets/Extensions/TransformExtensions.cs
+++ Assets/Extensions/TransformExtensions.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A static class containing extension methods for <see cref="Transform"/> objects.
+/// </summary>
 public static class TransformExtensions
 {
+    /// <summary>
+    /// Finds the immediate children of this transform with the specified tag.
+    /// </summary>
+    /// <param name="transform">The transform with child objects to be searched.</param>
+    /// <param name="tag">The tag to search with.</param>
+    /// <returns></returns>
     public static IEnumerable<Transform> GetChildObjectsWithTag(this Transform transform, string tag)
     {
         var taggedChildren = new List<Transform>();
