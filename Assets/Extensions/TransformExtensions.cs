599ed05b72c01b6a5174ab0dfcfd2a98dbecb64e
Dan McElroy <daniel@mcelroys.net>
2018-01-17 20:10:01 +0000

    Refactored TransformExtensions, added GetComponentOfImmediateChildren.

--- Assets/Extensions/TransformExtensions.cs
+++ Assets/Extensions/TransformExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -13,16 +14,42 @@ public static class TransformExtensions
     /// <param name="tag">The tag to search with.</param>
     /// <returns></returns>
     public static IEnumerable<Transform> GetImmediateChildrenWithTag(this Transform transform, string tag)
+        => transform.SelectImmediateChildrenWhere(
+            child => child.tag == tag,
+            child => child);
+    
+    /// <summary>
+    /// Finds components of the specified type within the transform's immediate children.
+    /// </summary>
+    /// <typeparam name="T">The type of component required.</typeparam>
+    /// <param name="transform">The transform with child objects to be searched.</param>
+    /// <returns></returns>
+    public static IEnumerable<T> GetComponentOfImmediateChildren<T>(this Transform transform)
+        => transform.SelectImmediateChildrenWhere(
+            child => child.GetComponent<T>() != null, 
+            child => child.GetComponent<T>());
+
+    /// <summary>
+    /// Filters the transform's immediate children by a predicate function, and
+    /// projects each of these children into the specified type via the selector function.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="transform">The transform with child objects to be selected.</param>
+    /// <param name="predicate">A function to filter children by.</param>
+    /// <param name="selector">A function to select the required value from a child.</param>
+    /// <returns></returns>
+    private static IEnumerable<T> SelectImmediateChildrenWhere<T>(this Transform transform, 
+        Func<Transform, bool> predicate, Func<Transform, T> selector)
     {
-        var taggedChildren = new List<Transform>();
+        var selected = new List<T>();
         for (var i = 0; i < transform.childCount; i++)
         {
             var child = transform.GetChild(i);
-            if (child.tag == tag)
+            if (predicate(child))
             {
-                taggedChildren.Add(child);
+                selected.Add(selector(child));
             }
         }
-        return taggedChildren;
+        return selected;
     }
 }
