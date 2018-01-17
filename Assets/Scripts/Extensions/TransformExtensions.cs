45ab403750301fcbed6aaa50a97948df93cb682d
Dan McElroy <daniel@mcelroys.net>
2018-01-17 18:33:26 +0000

    Restructured folders.

---
+++ Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanMinerTycoon.Extensions
+{
+    /// <summary>
+    /// A static class containing extension methods for <see cref="Transform"/> objects.
+    /// </summary>
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Finds the immediate children of this transform with the specified tag.
+        /// </summary>
+        /// <param name="transform">The transform with child objects to be searched.</param>
+        /// <param name="tag">The tag to search with.</param>
+        /// <returns></returns>
+        public static IEnumerable<Transform> GetChildObjectsWithTag(this Transform transform, string tag)
+        {
+            var taggedChildren = new List<Transform>();
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                var child = transform.GetChild(i);
+                if (child.tag == tag)
+                {
+                    taggedChildren.Add(child);
+                }
+            }
+            return taggedChildren;
+        }
+    }
+}
